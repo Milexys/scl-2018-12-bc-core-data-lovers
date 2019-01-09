e6be76173baad9e5657b0b5f8c2e35266ce83697
Milexys Ferrer <milexys.ferrerp@gmail.com>
2019-01-09 20:16:21 -0300

    adding test to order function

--- test/data.spec.js
+++ test/data.spec.js
@@ -20,5 +20,21 @@ describe('POKEMON.pokemon.filterPokemon', () => {
     expect(window.pokemones.filterPokemon(POKEMON.pokemon, "Fighting")[1].name).toEqual('Primeape');
   });
 });
-
+describe('POKEMON.pokemon.orderPokemon', () => {
+  it('it should be a function', ()=> {
+    expect(typeof window.pokemones.orderPokemon).toBe('function');
+  });
+  it('it should return the pokemon name: Abra from the first array with order A-Z', () => {
+    expect(window.pokemones.orderPokemon(POKEMON.pokemon, "name", "az")[0].name).toEqual('Abra');
+  });
+  it('it should return the pokemon name: Zubat from the first array with order Z-A', () => {
+    expect(window.pokemones.orderPokemon(POKEMON.pokemon, "name", "za")[0].name).toEqual('Zubat');
+  });
+  it('it should return the pokemon num: 001 from the first array with order 1-151', () => {
+    expect(window.pokemones.orderPokemon(POKEMON.pokemon, "num", "small-big")[0].num).toEqual('001');
+  });
+  it('it should return the pokemon num: 151 from the first array with order 151-1', () => {
+    expect(window.pokemones.orderPokemon(POKEMON.pokemon, "num", "big-small")[0].num).toEqual('151');
+  });
+});
  
