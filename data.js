9ba671bd6b01c4116b7f9c5d4b6a244c5ac2318a
Milexys Ferrer <milexys.ferrerp@gmail.com>
2019-01-09 10:12:54 -0300

    adding order function

--- data.js
+++ data.js
@@ -9,3 +9,15 @@ const filterPokemon = (data , condition) => {
     });
     return filteredPokemons;
 };
+const orderPokemon = (data) => {
+    const orderedPokemon = data.sort(function(a, b){
+        if (a.name > b.name){
+            return 1;
+        }
+        else if (a.name < b.name){
+            return -1;
+        }
+        else return 0;
+    });
+    return orderedPokemon;
+};
