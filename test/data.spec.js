04efc7deef87b2ff74a54a08e37524a4f7e2f6c9
Milexys Ferrer <milexys.ferrerp@gmail.com>
2019-01-09 20:34:33 -0300

    adding const data and change POKEMON.pokemon

--- test/data.spec.js
+++ test/data.spec.js
@@ -3,38 +3,39 @@ global.assert = require('chai').assert;
 require('../src/data/pokemon/pokemon.js');
 require('../src/data.js');
 
-describe('POKEMON.pokemon', () => {
+const data = window.POKEMON.pokemon;
+
+describe('data', () => {
   it('debería ser un objeto', () => {
     expect(typeof window.pokemones).toBe('object');
   });
 });
 
-describe('POKEMON.pokemon.filterPokemon', () => {
+describe('filterPokemon', () => {
   it('it should be a function', ()=> {
     expect(typeof window.pokemones.filterPokemon).toBe('function');
   });
   it('it should return as first pokemon with a condition type Water Squirtle', () => {
-    expect(window.pokemones.filterPokemon(POKEMON.pokemon, "Water")[0].name).toEqual('Squirtle');
+    expect(window.pokemones.filterPokemon(data, "Water")[0].name).toEqual('Squirtle');
   });
   it('it should return as second pokemon with a condition type Fighting Primeape', () => {
-    expect(window.pokemones.filterPokemon(POKEMON.pokemon, "Fighting")[1].name).toEqual('Primeape');
+    expect(window.pokemones.filterPokemon(data, "Fighting")[1].name).toEqual('Primeape');
   });
 });
-describe('POKEMON.pokemon.orderPokemon', () => {
+describe('orderPokemon', () => {
   it('it should be a function', ()=> {
     expect(typeof window.pokemones.orderPokemon).toBe('function');
   });
   it('it should return the pokemon name: Abra from the first array with order A-Z', () => {
-    expect(window.pokemones.orderPokemon(POKEMON.pokemon, "name", "az")[0].name).toEqual('Abra');
+    expect(window.pokemones.orderPokemon(data, "name", "az")[0].name).toEqual('Abra');
   });
   it('it should return the pokemon name: Zubat from the first array with order Z-A', () => {
-    expect(window.pokemones.orderPokemon(POKEMON.pokemon, "name", "za")[0].name).toEqual('Zubat');
+    expect(window.pokemones.orderPokemon(data, "name", "za")[0].name).toEqual('Zubat');
   });
   it('it should return the pokemon num: 001 from the first array with order 1-151', () => {
-    expect(window.pokemones.orderPokemon(POKEMON.pokemon, "num", "small-big")[0].num).toEqual('001');
+    expect(window.pokemones.orderPokemon(data, "num", "small-big")[0].num).toEqual('001');
   });
   it('it should return the pokemon num: 151 from the first array with order 151-1', () => {
-    expect(window.pokemones.orderPokemon(POKEMON.pokemon, "num", "big-small")[0].num).toEqual('151');
+    expect(window.pokemones.orderPokemon(data, "num", "big-small")[0].num).toEqual('151');
   });
 });
- 
