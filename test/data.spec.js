a7becb59b15c3e0b45a87cf6143f4ac22c95f462
Milexys Ferrer <milexys.ferrerp@gmail.com>
2019-01-09 20:01:40 -0300

    adding other test to filter function

--- test/data.spec.js
+++ test/data.spec.js
@@ -13,10 +13,12 @@ describe('POKEMON.pokemon.filterPokemon', () => {
   it('it should be a function', ()=> {
     expect(typeof window.pokemones.filterPokemon).toBe('function');
   });
-  it('it should return arrays with a condition type Water', () => {
+  it('it should return as first pokemon with a condition type Water Squirtle', () => {
     expect(window.pokemones.filterPokemon(POKEMON.pokemon, "Water")[0].name).toEqual('Squirtle');
   });
-  
+  it('it should return as second pokemon with a condition type Fighting Primeape', () => {
+    expect(window.pokemones.filterPokemon(POKEMON.pokemon, "Fighting")[1].name).toEqual('Primeape');
+  });
 });
 
  
