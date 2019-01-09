09867a8d41a3331a79c48771b044e73f794379b9
Milexys Ferrer <milexys.ferrerp@gmail.com>
2019-01-09 19:46:59 -0300

    adding 3 parameters to the function OrderPokemon, and adjusting the function with de new parameters

--- src/data.js
+++ src/data.js
@@ -1,23 +1,61 @@
-const filterPokemon = (data , condition) => {
+window.pokemones ={
+filterPokemon: (data , condition) => {
     if (condition === "0") {
         return data;
         
-    };
+    }
     const filteredPokemons  = data.filter(element => {
         if (element.type[0] === condition) return element.type[0] === condition;
         else if (element.type[1] === condition) return element.type[1] === condition;
     });
     return filteredPokemons;
-};
-const orderPokemon = (data) => {
-    const orderedPokemon = data.sort(function(a, b){
-        if (a.name > b.name){
+},
+orderPokemon: (data, sortBy, sortOrder) => {
+    const dataPokemon = data
+    if (sortBy === "name" && sortOrder === "az"){
+        dataPokemon.sort((prev, next)=> {
+            if (prev.name > next.name) {
             return 1;
-        }
-        else if (a.name < b.name){
+            }
+            if (prev.name < next.name) {
+                return -1;
+            }
+            return 0;
+        });
+    }
+    if (sortBy === "name" && sortOrder === "za"){
+        dataPokemon.sort((prev, next)=> {
+            if (prev.name < next.name) {
+                return 1;
+            }
+            if (prev.name > next.name) {
+                return -1;
+            }
+            return 0;
+        })
+    }
+    if (sortBy === "num" && sortOrder === "small-big"){
+        dataPokemon.sort((prev, next)=> {
+            if (prev.id > next.id) {
+            return 1;
+            }
+            if (prev.id < next.id) {
+                return -1;
+            }
+            return 0;
+        })
+    }
+    if (sortBy === "num" && sortOrder === "big-small"){
+        dataPokemon.sort((prev, next)=> {
+            if (prev.id < next.id) {
+                return 1;
+            }
+            if (prev.id > next.id) {
             return -1;
-        }
-        else return 0;
-    });
-    return orderedPokemon;
-};
+            }
+            return 0;
+        })
+    }
+  return dataPokemon;
+ }
+}
